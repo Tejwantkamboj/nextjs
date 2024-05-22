1338dd6b62e0450e827e0eb9c2e7aa3dcba9b8ed
TejwantKamboj <tejwant2444@gmail.com>
2024-05-22 23:05:32 +0530

    added tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection"
 
 export default function Home() {
   return (
-  <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-  <h1 className="text-2xl text-center"></h1>
+  <main className="min-h-screen bg-white/[0.96] antialiased bg-grid-black/[0.02]">  
+  <HeroSection />
   </main>
   );
 }
